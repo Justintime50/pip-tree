4bfc9df21d8866bf587c061b8851b9bc6ae4674e
Justintime50 <justinpaulhammond@gmail.com>
2020-11-24 12:08:55 -0700

    v0.4.0

--- test/unit/test_tree.py
+++ test/unit/test_tree.py
@@ -7,15 +7,15 @@ from pip_tree import PipTree
 @mock.patch('pip_tree.tree.PipTree.generate_dependency_tree', return_value=['mock-output', 40])
 def test__generate_console_output(mock_dependency_tree):
     PipTree._generate_console_output()
-    mock_dependency_tree.assert_called_once_with('pip3')
+    mock_dependency_tree.assert_called_once()
 
 
 @mock.patch('pip_tree.tree.PipTree.get_pip_package_list', return_value=[{'name': 'mock-package'}])
 @mock.patch('pip_tree.tree.PipTree.get_package_dependencies', return_value=[{'Name': 'mock-package'}, 40])  # noqa
 def test_generate_dependency_tree(mock_get_package_dependencies, mock_get_pip_package_list):
     PipTree.generate_dependency_tree()
-    mock_get_pip_package_list.assert_called_once_with('pip3')
-    mock_get_package_dependencies.assert_called_once_with([{'name': 'mock-package'}], 'pip3')
+    mock_get_pip_package_list.assert_called_once()
+    mock_get_package_dependencies.assert_called_once_with([{'name': 'mock-package'}])
 
 
 @mock.patch('subprocess.check_output', side_effect=subprocess.TimeoutExpired(cmd=subprocess.check_output, timeout=0.1))  # noqa
