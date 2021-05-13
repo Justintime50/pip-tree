b030e1969969c149f2ca446cf9a627134abc739c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-05-13 12:48:46 -0600

    Updates tests

--- test/unit/test_tree.py
+++ test/unit/test_tree.py
@@ -10,10 +10,19 @@ def get_python_version():
 
 
 @mock.patch('pip_tree.tree.PIP_PATH', f'./venv/lib/python{get_python_version()}/site-packages')
-def test_get_package_details(expected_tree_output, expected_package_count):
+def test_get_package_details():
+    """The poor man's approach to unit testing this library is to feed
+    its own virtual environment details to itself and assert they match.
+
+    Asserting the entire collection is difficult due to odd environment
+    dependencies (such as pip itself being a different version in build systems),
+    simply assert that certain attributes exist for a package such as `pytest`.
+    """
     package_details, package_count = PipTree.generate_pip_tree()
-    assert expected_tree_output in package_details
-    assert package_count == expected_package_count
+    assert any(item['name'] == 'pytest-cov' for item in package_details)
+    assert any('toml' in item['requires'] for item in package_details)
+    assert any('pytest' in item['required_by'] for item in package_details)
+    assert package_count == 25  # The number of dependencies that are contained in this virtual env.
 
 
 @mock.patch('pip_tree.tree.PIP_PATH', None)
