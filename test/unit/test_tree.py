409faef5ddf4c9aed5981ede4055d6d64cc3dc63
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-04-21 00:36:04 -0600

    fix: tests

--- test/unit/test_tree.py
+++ test/unit/test_tree.py
@@ -22,4 +22,4 @@ def test_get_package_details():
     assert any(item['name'] == 'pytest' for item in package_details)
     assert any([] == item['requires'] for item in package_details)
     assert any('pytest' in item['required_by'] for item in package_details)
-    assert 20 < package_count < 40  # Assert the package count is within a reasonable number for this package, ~30
+    assert 30 < package_count < 60  # Assert the package count is within a reasonable number for this package
